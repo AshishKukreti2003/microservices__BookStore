3652514dbcf4ea876b85231e06d1343be5052b20
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-09-29 22:53:18 +0530

    first commit

--- HotelService/src/main/java/com/op/hotel/repositories/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.op.hotel.repositories;
-
-import com.op.hotel.entites.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-
-    
-}
